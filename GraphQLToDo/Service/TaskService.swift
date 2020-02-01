3d3f2800bc905ed8ded2ee4c014f9e972abb9143
Moriya-Taichi <minecraft0125@gmail.com>
2020-02-02 04:45:53 +0900

    implement: Setup TaskService and fetchTasks

--- GraphQLToDo/Service/TaskService.swift
+++ GraphQLToDo/Service/TaskService.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import RxSwift
+
+protocol TaskServiceType {
+
+}
+
+final class TaskService: TaskServiceType {
+
+    let repository: TaskRepositoryType
+
+    init(repository: TaskRepositoryType) {
+        self.repository = repository
+    }
+
+    func fetchTasks(
+        completed: Bool? = nil,
+        order: TaskOrderFields,
+        endSursor: String,
+        hasNext: Bool
+    )
+        -> Observable<Pagination<TaskFields>>
+    {
+        let page = endSursor.isEmpty ? PaginationInput(first: 20) : PaginationInput(first: 10,
+                                                                                    after: endSursor)
+        return repository.fetchTasks(input: TasksInput(completed: completed),
+                                     orderBy: order,
+                                     page: page)
+            .asObservable()
+            .map { $0.tasks }
+            .map { response in
+                let elements = response.edges.compactMap { $0?.node.fragments.taskFields }
+                return Pagination(
+                    pageElements: elements,
+                    hasNextPage: response.pageInfo.fragments.pageInfoFields.hasNextPage,
+                    endCursor: response.pageInfo.fragments.pageInfoFields.endCursor
+                )
+        }
+    }
+}
