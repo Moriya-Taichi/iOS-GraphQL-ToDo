30d13ee480cd84ce963110e0adaa474acb7cf59e
Moriya-Taichi <minecraft0125@gmail.com>
2020-02-03 14:39:07 +0900

    implement: Storyboard instantiate

--- GraphQLToDo/Extension/UIViewController+instantiate.swift
+++ GraphQLToDo/Extension/UIViewController+instantiate.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum StoryboardName: String {
+    case tasks = "Tasks"
+    case task  = "Task"
+    case createTask = "CreateTask"
+}
+
+protocol StoryboardInstantiate {
+    static var storyboardName: StoryboardName { get }
+}
+
+extension StoryboardInstantiate where Self: UIViewController {
+    static func instantiate() -> Self {
+        let storyboard = UIStoryboard(name: self.storyboardName.rawValue, bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: String(describing: self)) as! Self
+        let backButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        viewController.navigationItem.backBarButtonItem = backButtonItem
+        return viewController
+    }
+}
