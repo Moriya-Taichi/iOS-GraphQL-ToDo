ea178a1113b03eb77001864d392a6f4ecfe6cfba
Moriya-Taichi <minecraft0125@gmail.com>
2020-02-04 23:46:45 +0900

    implement: RxDataSources SectionModel

--- GraphQLToDo/CellModel/SectionModel.swift
+++ GraphQLToDo/CellModel/SectionModel.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import Foundation
+import RxDataSources
+
+typealias CellItemSection = AnimatableSectionModel<SectionID, CellItem>
+
+protocol Identifiable: IdentifiableType, Equatable {
+    associatedtype Identifier: Equatable
+    var identity: Identifier { get }
+}
+
+enum SectionID: String, IdentifiableType {
+
+    case task
+
+    var identity: String {
+        return self.rawValue
+    }
+}
