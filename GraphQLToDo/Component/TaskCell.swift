cb613cb52fdb08e89d3c6317bf26c6bf7218a8c8
Moriya-Taichi <minecraft0125@gmail.com>
2020-02-09 00:08:09 +0900

    implement: TaskCell setcontent, bind logic

--- GraphQLToDo/Component/TaskCell.swift
+++ GraphQLToDo/Component/TaskCell.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+import UIKit
+
+final class TaskCell: UITableViewCell {
+
+    var disposeBag  = DisposeBag()
+
+    @IBOutlet private weak var checkCompletedButton: UIButton! {
+        didSet {
+            checkCompletedButton.setImage(nil, for: .normal)
+            checkCompletedButton.setImage(nil, for: .selected)
+        }
+    }
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var noteLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    private func setCellContents(task: TaskFields)  {
+        self.checkCompletedButton.isSelected = task.completed
+        self.titleLabel.text = task.title
+        self.noteLabel.text = task.notes
+        self.dateLabel.text = task.due
+    }
+}
+
+extension TaskCell: StoryboardView {
+    func bind(reactor: TaskCellReactor) {
+
+        reactor.state.map { $0.task }
+            .distinctUntilChanged()
+            .subscribe(onNext: { [weak self] task in
+                self?.setCellContents(task: task)
+            })
+            .disposed(by: disposeBag)
+
+        checkCompletedButton.rx
+            .tap
+            .map{ _ in Reactor.Action.complete }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
+}
