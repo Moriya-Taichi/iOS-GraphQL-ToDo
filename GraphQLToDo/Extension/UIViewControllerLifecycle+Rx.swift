2bf33a389c37285bc297c26ce8cda70b78a2305b
Moriya-Taichi <minecraft0125@gmail.com>
2020-02-16 05:17:02 +0900

    implement: Add Reactive LifeCycle Logic

--- GraphQLToDo/Extension/UIViewControllerLifecycle+Rx.swift
+++ GraphQLToDo/Extension/UIViewControllerLifecycle+Rx.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import UIKit
+
+extension Reactive where Base: UIViewController {
+
+    var viewDidLoad: Observable<Void> {
+        return self.sentMessage(#selector(Base.viewDidLoad))
+            .map { _ in }
+            .share(replay: 1, scope: .forever)
+    }
+
+    var viewWillApper: Observable<Void> {
+        return self.sentMessage(#selector(Base.viewDidLoad))
+            .map { _ in }
+            .share(replay: 1, scope: .forever)
+    }
+
+    var viewDidApper: Observable<Void> {
+        return self.sentMessage(#selector(Base.viewDidAppear))
+            .map { _ in }
+            .share(replay: 1, scope: .forever)
+    }
+
+    var viewWillDisApper: Observable<Void> {
+        return self.sentMessage(#selector(Base.viewWillDisappear))
+            .map { _ in }
+            .share(replay: 1, scope: .forever)
+    }
+
+    var viewDidDisApper: Observable<Void> {
+        return self.sentMessage(#selector(Base.viewDidDisappear))
+            .map { _ in }
+            .share(replay: 1, scope: .forever)
+    }
+}
+
